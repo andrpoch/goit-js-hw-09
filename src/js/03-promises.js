82f51485a9364c9d467153635d08fbcb2e6daab7
andrpoch <andrpoch25@gmail.com>
2022-02-13 00:38:40 +0200

    Completed third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formSubmit = document.querySelector('.form');
+
+formSubmit.addEventListener("submit", submitPromise);
+
+function submitPromise(event) {
+  event.preventDefault();
+    let {
+    elements: { delay, step, amount },
+    } = event.currentTarget;
+  delay = Number(delay.value);
+  step = Number(step.value);
+  amount = Number(amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        }, delay);
+      });
+    delay += step;
   }
 }
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+  });
+  
+};
